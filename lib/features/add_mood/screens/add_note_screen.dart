6be397720f8cb15901336ce22640d7864eba5e7b
Lewis <lewisnuikweh@gmail.com>
2025-06-08 15:55:00 +0100

    ui: fix scroll overflow

--- lib/features/add_mood/screens/add_note_screen.dart
+++ lib/features/add_mood/screens/add_note_screen.dart
@@ -60,105 +60,107 @@ class _AddNoteScreenState extends State<AddNoteScreen> {
             ),
           ),
 
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: Dimensions.padding16,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CustomAppBar(
-                  pageTitle: "Add Feeling",
-                  showBackButton: true,
-                  showTitle: false,
-                ),
-
-                SvgPicture.asset(
-                  widget.feeling.shape,
-                  height: size.width * .5,
-                  width: size.width * .5,
-                ),
-
-                // .animate()
-                // .moveY(
-                //   duration: 800.ms,
-                //   begin: 0,
-                //   end: -200,
-                //   curve: Spring.defaultIOS.toCurve,
-                // )
-                // .blur(
-                //   duration: 600.ms,
-                //   begin: Offset(0, 0),
-                //   end: Offset(15, 15),
-                // )
-                // .fadeOut(duration: 600.ms, delay: 200.ms),
-                Gap(Dimensions.padding16),
-                Spacer(),
-                Text(
-                  "Feeling ${widget.feeling.label}",
-                  style: theme.textTheme.titleLarge?.copyWith(
-                    color: theme.colorScheme.primary,
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: Dimensions.padding16,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CustomAppBar(
+                    pageTitle: "Add Feeling",
+                    showBackButton: true,
+                    showTitle: false,
                   ),
-                ),
-                Gap(Dimensions.padding16),
-                FittedBox(
-                  child: AppButton(
+
+                  SvgPicture.asset(
+                    widget.feeling.shape,
+                    height: size.width * .5,
+                    width: size.width * .5,
+                  ),
+
+                  // .animate()
+                  // .moveY(
+                  //   duration: 800.ms,
+                  //   begin: 0,
+                  //   end: -200,
+                  //   curve: Spring.defaultIOS.toCurve,
+                  // )
+                  // .blur(
+                  //   duration: 600.ms,
+                  //   begin: Offset(0, 0),
+                  //   end: Offset(15, 15),
+                  // )
+                  // .fadeOut(duration: 600.ms, delay: 200.ms),
+                  Gap(Dimensions.padding16),
+                  //Spacer(),
+                  Text(
+                    "Feeling ${widget.feeling.label}",
+                    style: theme.textTheme.titleLarge?.copyWith(
+                      color: theme.colorScheme.primary,
+                    ),
+                  ),
+                  Gap(Dimensions.padding16),
+                  FittedBox(
+                    child: AppButton(
+                      onPressed: () {
+                        //Todo: Implement adding other feelings
+                      },
+                      label: "Add another feeling",
+                      buttonType: ButtonType.secondary,
+                    ),
+                  ),
+                  Gap(Dimensions.padding16),
+                  //Spacer(),
+
+                  Text(
+                    "What's making you feel ${widget.feeling.label.toLowerCase()}?",
+                    style: theme.textTheme.titleLarge,
+                  ),
+
+                  Gap(Dimensions.padding32),
+                  Container(
+                    decoration: BoxDecoration(
+                      boxShadow: isDarkMode ? [
+                        BoxShadow(
+                          blurRadius: 12,
+                          spreadRadius: 1,
+                          color: theme.colorScheme.onSurface.withValues(alpha: .05),
+                        ),
+                      ] : null
+                    ),
+                    child: TextFormField(
+                      controller: textEditingController,
+                      focusNode: _focusNode,
+                      // onTapOutside: (event){
+                      //     _focusNode.unfocus();
+                      // },
+                      maxLines: 5,
+                      decoration: AppInputDecoration.inputDecoration(
+                        context,
+                      ).copyWith(hintText: "Write something"),
+                    ),
+                  ),
+                  Gap(Dimensions.padding24),
+                  AppButton(
                     onPressed: () {
-                      //Todo: Implement adding other feelings
+                      //TODO: Perform actions such as saving the note.
+
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder:
+                              (context) =>
+                                  SelectActivitiesScreen(feeling: widget.feeling),
+                        ),
+                      );
                     },
-                    label: "Add another feeling",
-                    buttonType: ButtonType.secondary,
+                    label: "Continue",
                   ),
-                ),
-                Gap(Dimensions.padding16),
-                Spacer(),
-
-                Text(
-                  "What's making you feel ${widget.feeling.label.toLowerCase()}?",
-                  style: theme.textTheme.titleLarge,
-                ),
-
-                Gap(Dimensions.padding32),
-                Container(
-                  decoration: BoxDecoration(
-                    boxShadow: isDarkMode ? [
-                      BoxShadow(
-                        blurRadius: 12,
-                        spreadRadius: 1,
-                        color: theme.colorScheme.onSurface.withValues(alpha: .05),
-                      ),
-                    ] : null
-                  ),
-                  child: TextFormField(
-                    controller: textEditingController,
-                    focusNode: _focusNode,
-                    onTapOutside: (event){
-                        _focusNode.unfocus();
-                    },
-                    maxLines: 5,
-                    decoration: AppInputDecoration.inputDecoration(
-                      context,
-                    ).copyWith(hintText: "Write something"),
-                  ),
-                ),
-                Gap(Dimensions.padding24),
-                AppButton(
-                  onPressed: () {
-                    //TODO: Perform actions such as saving the note.
-
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder:
-                            (context) =>
-                                SelectActivitiesScreen(feeling: widget.feeling),
-                      ),
-                    );
-                  },
-                  label: "Continue",
-                ),
-                Gap(Dimensions.padding64),
-              ],
+                  Gap(Dimensions.padding64),
+                ],
+              ),
             ),
           ),
         ],
